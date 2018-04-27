1c09df1340341bf5270e1c2df75cf3bf40547454
amadeufrn <amadeufrn@gmail.com>
2018-04-27 09:30:59 -0400

    modificacao de contato

--- projetophp/index.php
+++ projetophp/index.php
@@ -2,9 +2,27 @@
 <html>
 <body>
 
-<?php
-echo "My first PHP script!";
-?>
+    <?php
+  
+//*  $texto = "http://www.google.com";
+//*     echo " <a href ='".$texto."'> Clique Aqui </a>";
+//*     ?> 
+  
+   <script>
+  
+//*   var valor = 10 + 35;
+  //*   alert(valor);
+  
+ //*  </script>
+  
+  
+  <form action="/cadastra.php">
+  Nome:<br>
+  <input type="text" name="name" value=><br>
+  Email:<br>
+  <input type="text" name="email" value=><br><br>
+  <input type="submit" value="Submit">
+</form>
 
 </body>
 </html>
